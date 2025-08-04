284689c5318233183c980278bab2c99333e47957
debajyatibanerjee <techsupport10@drivado.com>
2025-08-04 14:14:35 +0530

    add product model

---
+++ internal/domain/product/model.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID          uuid.UUID `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Description string    `json:"description" db:"description"`
+	Price       float64   `json:"price" db:"price"`
+	Stock       int       `json:"stock" db:"stock"`
+	Active      bool      `json:"active" db:"active"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type CreateProductRequest struct {
+	Name        string  `json:"name" binding:"required"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price" binding:"required, gt=0"`
+	Stock       int     `json:"stock" binding:"required, gte=0"`
+}
+
+type UpdateProductRequest struct {
+	Name        *string  `json:"name,omitempty"`
+	Description *string  `json:"description,omitempty"`
+	Price       *float64 `json:"price,omitempty"`
+	Stock       *int     `json:"stock,omitempty"`
+}
